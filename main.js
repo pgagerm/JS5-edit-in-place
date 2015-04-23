7ad33ad176bf08c1807c2bc6147de4e6aaa08907
Michael Germany <mike@rickgermany.com>
2015-04-23 14:22:47 -0500

    created editable fields

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
 $(document).on('ready', function() {
-  
+  $('.edit').on('click', function(){
+
+    var originalField = $(this);
+    var input = $('<textarea class="edit-input" />');
+
+    $(this).after(input);
+
+    input.height($(this).height());
+    $(this).hide();
+
+    input.val($(this).text());
+
+    input.focus();
+
+    input.on('blur', function(){
+
+      originalField.text(input.val());
+
+      input.remove();
+
+      originalField.show();
+
+    });
+
+  });
+
 });
